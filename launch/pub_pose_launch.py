130df4313ff0efdd25d03f14a78590d017828f1c
shimpei <shimpei0b@gmail.com>
2023-09-21 21:43:33 +0900

    new

--- launch/pub_pose_launch.py
+++ launch/pub_pose_launch.py
@@ -1,4 +1,27 @@
 import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        # 既存の launch ファイルのコンポーネントをここに追加
+
+        # 新しいコマンドを実行するアクションを追加
+        launch.actions.ExecuteProcess(
+            cmd=["ros2", "topic", "pub", "-1", "/initialpose", "geometry_msgs/PoseWithCovarianceStamped",
+                 "{ header: {stamp: {sec: 0, nanosec: 0}, frame_id: 'map'}, pose: { pose: {position: {x: 0.0, y: 0.0, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 0.0}}, } }"],
+            output='screen',  # コマンドの出力をターミナルに表示
+        )
+
+        launch.actions.ExecuteProcess(
+            cmd=["rviz2"],
+            output='screen',  # コマンドの出力をターミナルに表示
+        )
+    ])
+
+"""
+import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
@@ -14,3 +37,4 @@ def generate_launch_description():
             # 例: parameters=[{'param_name': 'param_value'}]
         ),
     ])
+"""
