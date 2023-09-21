febfa941943aa744a8ae64c9e53e9fbe85215883
shimpei <shimpei0b@gmail.com>
2023-09-21 21:53:04 +0900

    new

--- launch/pub_pose_launch.py
+++ launch/pub_pose_launch.py
@@ -1,4 +1,24 @@
 import launch
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='pub_initialpose',  # ノードのパッケージ名を指定
+            executable='publish_initialpose',  # ノードの実行可能ファイル名を指定
+            output='screen',  # ログ出力設定
+            name='pub_pose_node',  # ノード名を指定
+            # 他のパラメーターやremappingを追加
+            # 例: remappings=[('/input_topic', '/new_input_topic')]
+            # 例: parameters=[{'param_name': 'param_value'}]
+        ),
+    ])
+
+
+
+"""
+import launch
 import launch.actions
 import launch.substitutions
 import launch_ros.actions
@@ -14,27 +34,5 @@ def generate_launch_description():
             output='screen',  # コマンドの出力をターミナルに表示
         )
 
-        launch.actions.ExecuteProcess(
-            cmd=["rviz2"],
-            output='screen',  # コマンドの出力をターミナルに表示
-        )
-    ])
-
-"""
-import launch
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='pub_initialpose',  # ノードのパッケージ名を指定
-            executable='publish_initialpose',  # ノードの実行可能ファイル名を指定
-            output='screen',  # ログ出力設定
-            name='pub_pose_node',  # ノード名を指定
-            # 他のパラメーターやremappingを追加
-            # 例: remappings=[('/input_topic', '/new_input_topic')]
-            # 例: parameters=[{'param_name': 'param_value'}]
-        ),
     ])
 """
